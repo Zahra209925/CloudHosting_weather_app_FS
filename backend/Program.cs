5ec96ae8458d1bf8198ec4f113f2e7da2ac0d114
Zahra209925 <nabavizahra2099@gmail.com>
2025-08-17 12:17:36 +0300

    päivitetty kaikki tiedostod.

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!"); //  using "anonomous function
+app.MapGet("hellous/", GetHello);
 
 app.Run();
+
+string GetHello()
+{
+	var helloFolder = new DirectoryInfo(Directory.GetCurrentDirectory());
+	var hellopath = Path.Combine(helloFolder.FullName, "hello.txt");
+
+	// print to console absolute path (fullname)
+
+	Console.WriteLine($"reading hello from :{hellopath}");
+
+	var message = File.ReadAllText(hellopath);
+	return "Read from FILE:\n\n" + message;
+
+}
