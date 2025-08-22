85432a8f866cf512ecec0fd8c47f6e3295c53343
Zahra209925 <nabavizahra2099@gmail.com>
2025-08-22 12:26:48 +0300

    päivitetty tiedostod.

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,21 +1,34 @@
+//1)
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!"); //  using "anonomous function
+//2)
+// we may need to configure builder before building  (as an example, add controllers etc.)
+
+//3)
+var app = builder.Build();
+// 4)
+// MAP. ENDPOINT <- > metodi()
+app.MapGet("/", () => "Hello NET24S!"); //  using "anonomous function
 app.MapGet("hellous/", GetHello);
 
+// 5). after run .... program will stop here to wait for Get/POST/UPDATE calls
 app.Run();
+//--------------------------------------------------
 
+Console.WriteLine("This should never happen ... (is impossible, should be at least)");
+// we will never get here..
+
+// what about these??
 string GetHello()
 {
 	var helloFolder = new DirectoryInfo(Directory.GetCurrentDirectory());
 	var hellopath = Path.Combine(helloFolder.FullName, "hello.txt");
 
 	// print to console absolute path (fullname)
-
-	Console.WriteLine($"reading hello from :{hellopath}");
+	Console.WriteLine($"Reading hello from :{hellopath}");
 
 	var message = File.ReadAllText(hellopath);
 	return "Read from FILE:\n\n" + message;
 
 }
+
